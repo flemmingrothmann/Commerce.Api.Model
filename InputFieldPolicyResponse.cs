b78c0c8fc4ccb2711267a04440011bb8bba42b38
Flemming Rothmann <fro@fiftytwo.com>
2020-06-10 13:56:41 +0200

    InputFieldPolicyResponse wsa attributed incorrectly.

--- InputFieldPolicyResponse.cs
+++ InputFieldPolicyResponse.cs
@@ -6,10 +6,10 @@ namespace Commerce.Api.Model
     public class InputFieldPolicyResponse
     {
         public string FieldName { get; set; }
-        public string Kind { get; set; }
-
         [JsonConverter(typeof(StringEnumConverter))]
-        public KindOfField FieldPolicy { get; set; }
+        public KindOfField Kind { get; set; }
+
+        public string FieldPolicy { get; set; }
 
         public string Lookup { get; set; }
         public string RegExValidation { get; set; }
