72cbb02a697bbc0270b3380761eae65ed043f8d5
Flemming Rothmann <fr@knalloo.dk>
2020-06-18 16:22:05 +0200

    Suppression of all "CA2227:Collection properties should be read only" warnings in Model - it is required to have a Set'er due to deserialization.

--- src/CommerceClient.Api.Model/GlobalSuppressions.cs
+++ src/CommerceClient.Api.Model/GlobalSuppressions.cs
@@ -5,283 +5,42 @@
 
 using System.Diagnostics.CodeAnalysis;
 
-[assembly: SuppressMessage(
-    "Usage",
-    "CA2227:Collection properties should be read only",
-    Justification = "Json serializer needs the setter",
-    Scope = "member",
-    Target = "~P:CommerceClient.Api.Model.Address.Constraints"
-)]
-[assembly: SuppressMessage(
-    "Usage",
-    "CA2227:Collection properties should be read only",
-    Justification = "Json serializer needs the setter",
-    Scope = "member",
-    Target = "~P:CommerceClient.Api.Model.BasketLineResponse.Links"
-)]
-[assembly: SuppressMessage(
-    "Usage",
-    "CA2227:Collection properties should be read only",
-    Justification = "Json serializer needs the setter",
-    Scope = "member",
-    Target = "~P:CommerceClient.Api.Model.BasketRequisiteResponse.References"
-)]
-[assembly: SuppressMessage(
-    "Usage",
-    "CA2227:Collection properties should be read only",
-    Justification = "Json serializer needs the setter",
-    Scope = "member",
-    Target = "~P:CommerceClient.Api.Model.BasketRequisiteResponse.Requirements"
-)]
-[assembly: SuppressMessage(
-    "Usage",
-    "CA2227:Collection properties should be read only",
-    Justification = "Json serializer needs the setter",
-    Scope = "member",
-    Target = "~P:CommerceClient.Api.Model.BasketResponse.Links"
-)]
-[assembly: SuppressMessage(
-    "Usage",
-    "CA2227:Collection properties should be read only",
-    Justification = "Json serializer needs the setter",
-    Scope = "member",
-    Target = "~P:CommerceClient.Api.Model.BasketResponse.Payments"
-)]
-[assembly: SuppressMessage(
-    "Usage",
-    "CA2227:Collection properties should be read only",
-    Justification = "Json serializer needs the setter",
-    Scope = "member",
-    Target = "~P:CommerceClient.Api.Model.BasketResponse.Shipments"
-)]
-[assembly: SuppressMessage(
-    "Usage",
-    "CA2227:Collection properties should be read only",
-    Justification = "Json serializer needs the setter",
-    Scope = "member",
-    Target = "~P:CommerceClient.Api.Model.BasketResponse.ValidationMessages"
-)]
-[assembly: SuppressMessage(
-    "Usage",
-    "CA2227:Collection properties should be read only",
-    Justification = "Json serializer needs the setter",
-    Scope = "member",
-    Target = "~P:CommerceClient.Api.Model.BasketUserValueResponse.Links"
-)]
-[assembly: SuppressMessage(
-    "Usage",
-    "CA2227:Collection properties should be read only",
-    Justification = "Json serializer needs the setter",
-    Scope = "member",
-    Target = "~P:CommerceClient.Api.Model.Context.Links"
-)]
-[assembly: SuppressMessage(
-    "Usage",
-    "CA2227:Collection properties should be read only",
-    Justification = "Json serializer needs the setter",
-    Scope = "member",
-    Target = "~P:CommerceClient.Api.Model.Country.Links"
-)]
-[assembly: SuppressMessage(
-    "Usage",
-    "CA2227:Collection properties should be read only",
-    Justification = "Json serializer needs the setter",
-    Scope = "member",
-    Target = "~P:CommerceClient.Api.Model.Currency.Links"
-)]
-[assembly: SuppressMessage(
-    "Usage",
-    "CA2227:Collection properties should be read only",
-    Justification = "Json serializer needs the setter",
-    Scope = "member",
-    Target = "~P:CommerceClient.Api.Model.Customer.Links"
-)]
-[assembly: SuppressMessage(
-    "Usage",
-    "CA2227:Collection properties should be read only",
-    Justification = "Json serializer needs the setter",
-    Scope = "member",
-    Target = "~P:CommerceClient.Api.Model.Customer.ShipToAddresses"
-)]
-[assembly: SuppressMessage(
-    "Usage",
-    "CA2227:Collection properties should be read only",
-    Justification = "Json serializer needs the setter",
-    Scope = "member",
-    Target = "~P:CommerceClient.Api.Model.CustomerLogin.Links"
-)]
-[assembly: SuppressMessage(
-    "Usage",
-    "CA2227:Collection properties should be read only",
-    Justification = "Json serializer needs the setter",
-    Scope = "member",
-    Target = "~P:CommerceClient.Api.Model.DataItemsResponseBody`1.Items"
-)]
-[assembly: SuppressMessage(
-    "Usage",
-    "CA2227:Collection properties should be read only",
-    Justification = "Json serializer needs the setter",
-    Scope = "member",
-    Target = "~P:CommerceClient.Api.Model.DataItemsResponseBody`1.Messages"
-)]
-[assembly: SuppressMessage(
-    "Usage",
-    "CA2227:Collection properties should be read only",
-    Justification = "Json serializer needs the setter",
-    Scope = "member",
-    Target = "~P:CommerceClient.Api.Model.DataProductListResponseBody`1.Articles"
-)]
-[assembly: SuppressMessage(
-    "Usage",
-    "CA2227:Collection properties should be read only",
-    Justification = "Json serializer needs the setter",
-    Scope = "member",
-    Target = "~P:CommerceClient.Api.Model.DataProductListResponseBody`1.FilterFields"
-)]
-[assembly: SuppressMessage(
-    "Usage",
-    "CA2227:Collection properties should be read only",
-    Justification = "Json serializer needs the setter",
-    Scope = "member",
-    Target = "~P:CommerceClient.Api.Model.DataProductListResponseBody`1.ProductMenus"
-)]
-[assembly: SuppressMessage(
-    "Usage",
-    "CA2227:Collection properties should be read only",
-    Justification = "Json serializer needs the setter",
-    Scope = "member",
-    Target = "~P:CommerceClient.Api.Model.DataProductListResponseBody`1.UnitOfMeasures"
-)]
-[assembly: SuppressMessage(
-    "Usage",
-    "CA2227:Collection properties should be read only",
-    Justification = "Json serializer needs the setter",
-    Scope = "member",
-    Target = "~P:CommerceClient.Api.Model.DataProductListResponseBody`1.VariantDimensions"
-)]
-[assembly: SuppressMessage(
-    "Usage",
-    "CA2227:Collection properties should be read only",
-    Justification = "Json serializer needs the setter",
-    Scope = "member",
-    Target = "~P:CommerceClient.Api.Model.ErrorResponse.Errors"
-)]
-[assembly: SuppressMessage(
-    "Usage",
-    "CA2227:Collection properties should be read only",
-    Justification = "Json serializer needs the setter",
-    Scope = "member",
-    Target = "~P:CommerceClient.Api.Model.FavoriteAnnotation.Links"
-)]
-[assembly: SuppressMessage(
-    "Usage",
-    "CA2227:Collection properties should be read only",
-    Justification = "Json serializer needs the setter",
-    Scope = "member",
-    Target = "~P:CommerceClient.Api.Model.FilterFieldResponse.Values"
-)]
-[assembly: SuppressMessage(
-    "Usage",
-    "CA2227:Collection properties should be read only",
-    Justification = "Json serializer needs the setter",
-    Scope = "member",
-    Target = "~P:CommerceClient.Api.Model.Location.CollectTimes"
-)]
-[assembly: SuppressMessage(
-    "Usage",
-    "CA2227:Collection properties should be read only",
-    Justification = "Json serializer needs the setter",
-    Scope = "member",
-    Target = "~P:CommerceClient.Api.Model.Location.Links"
-)]
-[assembly: SuppressMessage(
-    "Usage",
-    "CA2227:Collection properties should be read only",
-    Justification = "Json serializer needs the setter",
-    Scope = "member",
-    Target = "~P:CommerceClient.Api.Model.Location.OpeningHours"
-)]
-[assembly: SuppressMessage(
-    "Usage",
-    "CA2227:Collection properties should be read only",
-    Justification = "Json serializer needs the setter",
-    Scope = "member",
-    Target = "~P:CommerceClient.Api.Model.Location.SpecialOpeningHours"
-)]
-[assembly: SuppressMessage(
-    "Usage",
-    "CA2227:Collection properties should be read only",
-    Justification = "Json serializer needs the setter",
-    Scope = "member",
-    Target = "~P:CommerceClient.Api.Model.PaymentResponse.Links"
-)]
-[assembly: SuppressMessage(
-    "Usage",
-    "CA2227:Collection properties should be read only",
-    Justification = "Json serializer needs the setter",
-    Scope = "member",
-    Target = "~P:CommerceClient.Api.Model.Product`1.CustomFields"
-)]
-[assembly: SuppressMessage(
-    "Usage",
-    "CA2227:Collection properties should be read only",
-    Justification = "Json serializer needs the setter",
-    Scope = "member",
-    Target = "~P:CommerceClient.Api.Model.Product`1.Links"
-)]
-[assembly: SuppressMessage(
-    "Usage",
-    "CA2227:Collection properties should be read only",
-    Justification = "Json serializer needs the setter",
-    Scope = "member",
-    Target = "~P:CommerceClient.Api.Model.Product`1.SalesPrices"
-)]
-[assembly: SuppressMessage(
-    "Usage",
-    "CA2227:Collection properties should be read only",
-    Justification = "Json serializer needs the setter",
-    Scope = "member",
-    Target = "~P:CommerceClient.Api.Model.Product`1.SalesUnit"
-)]
-[assembly: SuppressMessage(
-    "Usage",
-    "CA2227:Collection properties should be read only",
-    Justification = "Json serializer needs the setter",
-    Scope = "member",
-    Target = "~P:CommerceClient.Api.Model.Product`1.SalesUnitConstraints"
-)]
-[assembly: SuppressMessage(
-    "Usage",
-    "CA2227:Collection properties should be read only",
-    Justification = "Json serializer needs the setter",
-    Scope = "member",
-    Target = "~P:CommerceClient.Api.Model.Product`1.VariantValues"
-)]
-[assembly: SuppressMessage(
-    "Usage",
-    "CA2227:Collection properties should be read only",
-    Justification = "Json serializer needs the setter",
-    Scope = "member",
-    Target = "~P:CommerceClient.Api.Model.ProductListProductMenuResponse.Links"
-)]
-[assembly: SuppressMessage(
-    "Usage",
-    "CA2227:Collection properties should be read only",
-    Justification = "Json serializer needs the setter",
-    Scope = "member",
-    Target = "~P:CommerceClient.Api.Model.ProductListVariantDimensionResponse.Values"
-)]
-[assembly: SuppressMessage(
-    "Usage",
-    "CA2227:Collection properties should be read only",
-    Justification = "Json serializer needs the setter",
-    Scope = "member",
-    Target = "~P:CommerceClient.Api.Model.Setting.Aliases"
-)]
-[assembly: SuppressMessage(
-    "Usage",
-    "CA2227:Collection properties should be read only",
-    Justification = "Json serializer needs the setter",
-    Scope = "member",
-    Target = "~P:CommerceClient.Api.Model.ShipmentResponse.Links"
-)]
+[assembly: SuppressMessage("Usage", "CA2227:Collection properties should be read only", Justification = "<Pending>", Scope = "member", Target = "~P:CommerceClient.Api.Model.CustomerLoginResponse.Constraints")]
+[assembly: SuppressMessage("Usage", "CA2227:Collection properties should be read only", Justification = "<Pending>", Scope = "member", Target = "~P:CommerceClient.Api.Model.Address.Constraints")]
+[assembly: SuppressMessage("Usage", "CA2227:Collection properties should be read only", Justification = "<Pending>", Scope = "member", Target = "~P:CommerceClient.Api.Model.BasketLineResponse.Links")]
+[assembly: SuppressMessage("Usage", "CA2227:Collection properties should be read only", Justification = "<Pending>", Scope = "member", Target = "~P:CommerceClient.Api.Model.BasketRequisiteResponse.References")]
+[assembly: SuppressMessage("Usage", "CA2227:Collection properties should be read only", Justification = "<Pending>", Scope = "member", Target = "~P:CommerceClient.Api.Model.BasketRequisiteResponse.Requirements")]
+[assembly: SuppressMessage("Usage", "CA2227:Collection properties should be read only", Justification = "<Pending>", Scope = "member", Target = "~P:CommerceClient.Api.Model.BasketResponse.Links")]
+[assembly: SuppressMessage("Usage", "CA2227:Collection properties should be read only", Justification = "<Pending>", Scope = "member", Target = "~P:CommerceClient.Api.Model.BasketResponse.Payments")]
+[assembly: SuppressMessage("Usage", "CA2227:Collection properties should be read only", Justification = "<Pending>", Scope = "member", Target = "~P:CommerceClient.Api.Model.BasketResponse.Shipments")]
+[assembly: SuppressMessage("Usage", "CA2227:Collection properties should be read only", Justification = "<Pending>", Scope = "member", Target = "~P:CommerceClient.Api.Model.BasketResponse.ValidationMessages")]
+[assembly: SuppressMessage("Usage", "CA2227:Collection properties should be read only", Justification = "<Pending>", Scope = "member", Target = "~P:CommerceClient.Api.Model.BasketUserValueResponse.Links")]
+[assembly: SuppressMessage("Usage", "CA2227:Collection properties should be read only", Justification = "<Pending>", Scope = "member", Target = "~P:CommerceClient.Api.Model.Context.Links")]
+[assembly: SuppressMessage("Usage", "CA2227:Collection properties should be read only", Justification = "<Pending>", Scope = "member", Target = "~P:CommerceClient.Api.Model.Country.Links")]
+[assembly: SuppressMessage("Usage", "CA2227:Collection properties should be read only", Justification = "<Pending>", Scope = "member", Target = "~P:CommerceClient.Api.Model.Currency.Links")]
+[assembly: SuppressMessage("Usage", "CA2227:Collection properties should be read only", Justification = "<Pending>", Scope = "member", Target = "~P:CommerceClient.Api.Model.CustomerLoginResponse.Links")]
+[assembly: SuppressMessage("Usage", "CA2227:Collection properties should be read only", Justification = "<Pending>", Scope = "member", Target = "~P:CommerceClient.Api.Model.DataItemsResponseBody`1.Items")]
+[assembly: SuppressMessage("Usage", "CA2227:Collection properties should be read only", Justification = "<Pending>", Scope = "member", Target = "~P:CommerceClient.Api.Model.DataItemsResponseBody`1.Messages")]
+[assembly: SuppressMessage("Usage", "CA2227:Collection properties should be read only", Justification = "<Pending>", Scope = "member", Target = "~P:CommerceClient.Api.Model.DataProductListResponseBody`1.Articles")]
+[assembly: SuppressMessage("Usage", "CA2227:Collection properties should be read only", Justification = "<Pending>", Scope = "member", Target = "~P:CommerceClient.Api.Model.DataProductListResponseBody`1.FilterFields")]
+[assembly: SuppressMessage("Usage", "CA2227:Collection properties should be read only", Justification = "<Pending>", Scope = "member", Target = "~P:CommerceClient.Api.Model.DataProductListResponseBody`1.ProductMenus")]
+[assembly: SuppressMessage("Usage", "CA2227:Collection properties should be read only", Justification = "<Pending>", Scope = "member", Target = "~P:CommerceClient.Api.Model.DataProductListResponseBody`1.UnitOfMeasures")]
+[assembly: SuppressMessage("Usage", "CA2227:Collection properties should be read only", Justification = "<Pending>", Scope = "member", Target = "~P:CommerceClient.Api.Model.DataProductListResponseBody`1.VariantDimensions")]
+[assembly: SuppressMessage("Usage", "CA2227:Collection properties should be read only", Justification = "<Pending>", Scope = "member", Target = "~P:CommerceClient.Api.Model.ErrorResponse.Errors")]
+[assembly: SuppressMessage("Usage", "CA2227:Collection properties should be read only", Justification = "<Pending>", Scope = "member", Target = "~P:CommerceClient.Api.Model.FavoriteAnnotation.Links")]
+[assembly: SuppressMessage("Usage", "CA2227:Collection properties should be read only", Justification = "<Pending>", Scope = "member", Target = "~P:CommerceClient.Api.Model.FilterFieldResponse.Values")]
+[assembly: SuppressMessage("Usage", "CA2227:Collection properties should be read only", Justification = "<Pending>", Scope = "member", Target = "~P:CommerceClient.Api.Model.Location.CollectTimes")]
+[assembly: SuppressMessage("Usage", "CA2227:Collection properties should be read only", Justification = "<Pending>", Scope = "member", Target = "~P:CommerceClient.Api.Model.Location.Links")]
+[assembly: SuppressMessage("Usage", "CA2227:Collection properties should be read only", Justification = "<Pending>", Scope = "member", Target = "~P:CommerceClient.Api.Model.Location.OpeningHours")]
+[assembly: SuppressMessage("Usage", "CA2227:Collection properties should be read only", Justification = "<Pending>", Scope = "member", Target = "~P:CommerceClient.Api.Model.Location.SpecialOpeningHours")]
+[assembly: SuppressMessage("Usage", "CA2227:Collection properties should be read only", Justification = "<Pending>", Scope = "member", Target = "~P:CommerceClient.Api.Model.PaymentResponse.Links")]
+[assembly: SuppressMessage("Usage", "CA2227:Collection properties should be read only", Justification = "<Pending>", Scope = "member", Target = "~P:CommerceClient.Api.Model.Product`1.CustomFields")]
+[assembly: SuppressMessage("Usage", "CA2227:Collection properties should be read only", Justification = "<Pending>", Scope = "member", Target = "~P:CommerceClient.Api.Model.Product`1.Links")]
+[assembly: SuppressMessage("Usage", "CA2227:Collection properties should be read only", Justification = "<Pending>", Scope = "member", Target = "~P:CommerceClient.Api.Model.Product`1.SalesPrices")]
+[assembly: SuppressMessage("Usage", "CA2227:Collection properties should be read only", Justification = "<Pending>", Scope = "member", Target = "~P:CommerceClient.Api.Model.Product`1.SalesUnit")]
+[assembly: SuppressMessage("Usage", "CA2227:Collection properties should be read only", Justification = "<Pending>", Scope = "member", Target = "~P:CommerceClient.Api.Model.Product`1.SalesUnitConstraints")]
+[assembly: SuppressMessage("Usage", "CA2227:Collection properties should be read only", Justification = "<Pending>", Scope = "member", Target = "~P:CommerceClient.Api.Model.Product`1.VariantValues")]
+[assembly: SuppressMessage("Usage", "CA2227:Collection properties should be read only", Justification = "<Pending>", Scope = "member", Target = "~P:CommerceClient.Api.Model.ProductListProductMenuResponse.Links")]
+[assembly: SuppressMessage("Usage", "CA2227:Collection properties should be read only", Justification = "<Pending>", Scope = "member", Target = "~P:CommerceClient.Api.Model.ProductListVariantDimensionResponse.Values")]
+[assembly: SuppressMessage("Usage", "CA2227:Collection properties should be read only", Justification = "<Pending>", Scope = "member", Target = "~P:CommerceClient.Api.Model.Setting.Aliases")]
+[assembly: SuppressMessage("Usage", "CA2227:Collection properties should be read only", Justification = "<Pending>", Scope = "member", Target = "~P:CommerceClient.Api.Model.ShipmentResponse.Links")]
